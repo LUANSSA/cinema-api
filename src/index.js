a078e61cdb367da7880bb05a4aea9607050983f4
Luan Souza <alunoluansenai@gmail.com>
2024-05-04 22:22:03 -0300

    ✨ Novo recurso: Avanço no desenvolvimento do projeto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const EXPRESS = require("express");
 const MONGOOSE = require("mongoose");
 
+const HANDLEBARS = require('express-handlebars');
+
+
 // Variáveis de ambiente
 require("dotenv").config();
 
@@ -8,12 +11,23 @@ const APP = EXPRESS();
 // Porta
 const PORT = 3000;
 
+
+APP.engine("handlebars", HANDLEBARS.engine({
+    extname: "handlebars",
+    defaultLayout: false,
+    layoutsDir: "views/layouts/"
+}));
+
+// APP.set("view engine", "handlebars");
+// APP.set("views", __dirname + "/views");
+
+// Permite com que a aplicação leia requisições com dados em json
+APP.use(EXPRESS.json());
+
 // Uuario e senha do banco de dados
 const DB_USER = process.env.MONGODB_USER;
 const DB_PASSWORD = process.env.MONGODB_PASS;
 
-// Conexao com o banco de dados
-MONGOOSE.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@starwars-api.myaob3d.mongodb.net/?retryWrites=true&w=majority&appName=starwars-api`);
 
 const FILME = MONGOOSE.model("filmes", {
     titulo:"String",
@@ -24,13 +38,27 @@ const FILME = MONGOOSE.model("filmes", {
 
 });
 
-// GET
-APP.get("/", (req,res) =>{
-    res.send("<h1>Hello Word!</h1>");
+// GET | Index
+APP.get("/",(req,res) =>{
+    res.render("index");
 });
 
-// POST
-APP.post("/", (req,res)=>{
+
+// GET | Filme | Listar todos
+APP.get("/filme", async (req,res) =>{
+    const FILMES = await FILME.find();
+    res.send(FILMES);
+});
+
+// GET | Filme | Consulta por ID
+APP.get("/filme/:id", async (req,res) =>{
+    const FILME = await FILME.findById(req.params.id);
+    res.send(FILME);
+});
+
+// POST | Filme Inserir
+APP.post("/filme", async (req,res)=>{
+
     const NOVOFILME =  new FILME({
         titulo: req.body.titulo,
         descricao: req.body.descricao,
@@ -38,9 +66,16 @@ APP.post("/", (req,res)=>{
         video_url: req.body.video_url,
         filme_ano:req.body.filme_ano
     });
+
+    await NOVOFILME.save();
+    res.send(NOVOFILME);
 });
 
+// Servidor
 APP.listen(PORT, ()=>{
+    // Conexao com o banco de dados
+    MONGOOSE.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@starwars-api.myaob3d.mongodb.net/?retryWrites=true&w=majority&appName=starwars-api`);
+
     console.log(`Servidor StarWars API iniciado com sucesso!`);
     console.log(`URL: http://localhost:${PORT}`);
 });
